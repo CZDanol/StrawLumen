63bd777af4907e8c382d6d21362a81d5bce88436
Daniel Čejchan <czdanol@gmail.com>
2023-08-04 17:42:19 +0200

    Some reformatting

--- src/gui/mainwindow_songsmode.h
+++ src/gui/mainwindow_songsmode.h
@@ -12,102 +12,100 @@
 #include "rec/songsection.h"
 
 namespace Ui {
-	class MainWindow_SongsMode;
+    class MainWindow_SongsMode;
 }
 
 class MainWindow_SongsMode : public QWidget
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	explicit MainWindow_SongsMode(QWidget *parent = nullptr);
-	~MainWindow_SongsMode();
+    explicit MainWindow_SongsMode(QWidget *parent = nullptr);
+    ~MainWindow_SongsMode();
 
 public:
-	QWidget *menuWidget();
-	QMenu *importMenu();
-	QMenu *exportMenu();
+    QWidget *menuWidget();
+    QMenu *importMenu();
+    QMenu *exportMenu();
 
 public slots:
-	void editSong(qlonglong songId);
-	void requestDeleteSongs(const QVector<qlonglong> &songIds);
+    void editSong(qlonglong songId);
+    void requestDeleteSongs(const QVector<qlonglong> &songIds);
 
-	/// Returns false if the user does not wish do close the edit mode
-	bool askFinishEditMode();
+    /// Returns false if the user does not wish do close the edit mode
+    bool askFinishEditMode();
 
-	QVector<qlonglong> selectedSongIds();
+    QVector<qlonglong> selectedSongIds();
 
 private:
-	void setSongEditMode(bool set);
+    void setSongEditMode(bool set);
 
-	void updateSongManipulationButtonsEnabled();
-	void updateCopyChordsMenu();
-	void updateTeContentMenu();
-	void updateMoveChordActionsEnabled();
+    void updateSongManipulationButtonsEnabled();
+    void updateCopyChordsMenu();
+    void updateTeContentMenu();
+    void updateMoveChordActionsEnabled();
 
-	void insertSongSection(const SongSection &section, bool positionCursorInMiddle = false);
-	void contentSelectionMorph(const std::function<QString(QString)> &callback, bool onlySection);
-	void moveChords(bool right);
+    void insertSongSection(const SongSection &section, bool positionCursorInMiddle = false);
+    void contentSelectionMorph(const std::function<QString(QString)> &callback, bool onlySection);
+    void moveChords(bool right);
 
-	/// Returns position and content of a song section around position pos
-	QPair<int,QString> songSectionAroundPos(int pos);
+    /// Returns position and content of a song section around position pos
+    QPair<int,QString> songSectionAroundPos(int pos);
 
 private slots:
-	void fillSongData();
+    void fillSongData();
 
 private slots:
-	void onCurrentSongChanged(qlonglong songId, int prevRowId);
-	void onSelectionChanged();
-	void onSongListContextMenuRequested(const QPoint &globalPos);
-	void onTeContentContextMenuRequested(const QPoint &pos);
+    void onCurrentSongChanged(qlonglong songId, int prevRowId);
+    void onSelectionChanged();
+    void onSongListContextMenuRequested(const QPoint &globalPos);
+    void onTeContentContextMenuRequested(const QPoint &pos);
 
 private slots:
-	void on_btnNew_clicked();
-	void on_btnDiscardChanges_clicked();
-	void on_btnSaveChanges_clicked();
-	void on_btnEdit_clicked();
-	void on_actionDeleteSongs_triggered();
-	void on_lnSlideOrder_sigFocused();
-	void on_btnInsertChord_clicked();
-	void on_btnTransposeUp_clicked();
-	void on_btnTransposeDown_clicked();
-	void on_btnAddCustomSlideOrderItem_pressed();
-	void on_btnInsertSlideSeparator_clicked();
-	void on_actionImportOpenSongSong_triggered();
-	void on_btnCreateSongbook_clicked();
-	void on_actionCreateSongbookFromSelection_triggered();
-	void on_actionExportToLumen_triggered();
-	void on_actionImportFromLumen_triggered();
-	void on_actionPresentSongs_triggered();
-	void on_actionAddSongsToPlaylist_triggered();
-	void on_actionExportToOpenSong_triggered();
-	void on_btnCopyChords_pressed();
-	void on_actionDeleteChords_triggered();
-	void on_btnAutoFormat_clicked();
-	void on_actionMoveChordsRight_triggered();
-	void on_teContent_cursorPositionChanged();
-	void on_actionMoveChordsLeft_triggered();
-	void on_actionOnlyChords_triggered();
-	void on_btnBulkEdit_clicked();
-	void on_btnConvertChords_clicked();
-
-	void on_btnTransposeFlat_clicked();
-
-	void on_actionImportFromPowerPoint_triggered();
+    void on_btnNew_clicked();
+    void on_btnDiscardChanges_clicked();
+    void on_btnSaveChanges_clicked();
+    void on_btnEdit_clicked();
+    void on_actionDeleteSongs_triggered();
+    void on_lnSlideOrder_sigFocused();
+    void on_btnInsertChord_clicked();
+    void on_btnTransposeUp_clicked();
+    void on_btnTransposeDown_clicked();
+    void on_btnAddCustomSlideOrderItem_pressed();
+    void on_btnInsertSlideSeparator_clicked();
+    void on_actionImportOpenSongSong_triggered();
+    void on_btnCreateSongbook_clicked();
+    void on_actionCreateSongbookFromSelection_triggered();
+    void on_actionExportToLumen_triggered();
+    void on_actionImportFromLumen_triggered();
+    void on_actionPresentSongs_triggered();
+    void on_actionAddSongsToPlaylist_triggered();
+    void on_actionExportToOpenSong_triggered();
+    void on_btnCopyChords_pressed();
+    void on_actionDeleteChords_triggered();
+    void on_btnAutoFormat_clicked();
+    void on_actionMoveChordsRight_triggered();
+    void on_teContent_cursorPositionChanged();
+    void on_actionMoveChordsLeft_triggered();
+    void on_actionOnlyChords_triggered();
+    void on_btnBulkEdit_clicked();
+    void on_btnConvertChords_clicked();
+    void on_btnTransposeFlat_clicked();
+    void on_actionImportFromPowerPoint_triggered();
 
 private:
-	Ui::MainWindow_SongsMode *ui;
-	QMenu insertSectionMenu_, importMenu_, exportMenu_, songListContextMenu_, copyChordsMenu_, teContentMenu_;
+    Ui::MainWindow_SongsMode *ui;
+    QMenu insertSectionMenu_, importMenu_, exportMenu_, songListContextMenu_, copyChordsMenu_, teContentMenu_;
 
 private:
-	QRegularExpressionValidator slideOrderValidator_;
-	QCompleter *slideOrderCompleter_;
-	QStringListModel slideOrderCompleterModel_;
-	QMenu addCustomSlideOrderItemMenu_;
+    QRegularExpressionValidator slideOrderValidator_;
+    QCompleter *slideOrderCompleter_;
+    QStringListModel slideOrderCompleterModel_;
+    QMenu addCustomSlideOrderItemMenu_;
 
 private:
-	qlonglong currentSongId_ = -1;
-	bool isSongEditMode_ = false;
+    qlonglong currentSongId_ = -1;
+    bool isSongEditMode_ = false;
 
 };
 
--- src/job/asynccachemanager.cpp
+++ src/job/asynccachemanager.cpp
@@ -9,53 +9,53 @@ AsyncCacheManager *asyncCache = nullptr;
 
 AsyncCacheManager::AsyncCacheManager()
 {
-	cache_.setMaxCost(200 * 1024 * 1024);
+    cache_.setMaxCost(200 * 1024 * 1024);
 }
 
 QVariant AsyncCacheManager::request(const QString key, AsyncCacheManager::ProduceFunction produceFunction, QObject *callbackParent, const AsyncCacheManager::CallbackFunction &callback)
 {
-	AsyncCacheManagerRecord *rec = cache_[key];
+    AsyncCacheManagerRecord *rec = cache_[key];
 
-	if(!rec) {
-		rec = new AsyncCacheManagerRecord();
-		cache_.insert(key, rec);
+    if(!rec) {
+        rec = new AsyncCacheManagerRecord();
+        cache_.insert(key, rec);
 
-		QPointer<AsyncCacheManagerRecord> recPtr(rec);
-		QPointer<QObject> callbackParentPtr(callbackParent);
+        QPointer<AsyncCacheManagerRecord> recPtr(rec);
+        QPointer<QObject> callbackParentPtr(callbackParent);
 
-		QtConcurrent::run([=]{
-			CheckStornoFunction chsf = [recPtr]{
-				return recPtr.isNull();
-			};
+        (void) QtConcurrent::run([=]{
+            CheckStornoFunction chsf = [recPtr]{
+                return recPtr.isNull();
+            };
 
-			QVariant data;
-			int cost = 1;
-			bool result = false;
+            QVariant data;
+            int cost = 1;
+            bool result = false;
 
-			if(!chsf())
-				result = produceFunction(data, cost, chsf);
+            if(!chsf())
+                result = produceFunction(data, cost, chsf);
 
-			execOnMainThread([=]{
-				if(recPtr.isNull())
-					return;
+            execOnMainThread([=]{
+                if(recPtr.isNull())
+                    return;
 
-				if(!result || (callbackParent && callbackParentPtr.isNull()))
-					cache_.remove(key);
+                if(!result || (callbackParent && callbackParentPtr.isNull()))
+                    cache_.remove(key);
 
-				else {
-					recPtr->data = data;
+                else {
+                    recPtr->data = data;
 
-					cache_.take(key);
-					cache_.insert(key, recPtr.data(), cost);
+                    cache_.take(key);
+                    cache_.insert(key, recPtr.data(), cost);
 
-					if(callback)
-						callback();
-				}
-			});
-		});
+                    if(callback)
+                        callback();
+                }
+            });
+        });
 
-		return QVariant();
-	}
+        return QVariant();
+    }
 
-	return rec->data;
+    return rec->data;
 }
